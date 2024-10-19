0609364c20275d21b8e0717bd5c1ad4705235d34
pavank-v <pavankumar.v1301@gmail.com>
2024-10-19 15:37:47 +0530

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,64 @@
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
+import { useState } from 'react';
+import { heroVideo, smallHeroVideo } from '../utils';
+import { useEffect } from 'react';
+
+const Hero = () => {
+  const [videoSrc, setvideoSrc] = useState(window.
+    innerWidth <  760 ? smallHeroVideo : heroVideo)
+
+  const handleVideoSrcSet = () => {
+    if (window.innerWidth < 760){
+      setvideoSrc(smallHeroVideo)
+    }else{
+      setvideoSrc(heroVideo)
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('resize', handleVideoSrcSet);
+
+    return (() => {
+      window.removeEventListener('resize', handleVideoSrcSet)
+    })
+  }, [])
+
+  useGSAP(() =>{
+    gsap.to('.hero-title', {
+      delay: 2,
+      opacity: 1,
+    });
+    gsap.to('#cta',{
+      delay: 2,
+      opacity: 1,
+      y: -50,
+    });
+  }, [])
+
+  return (
+    <section className="w-full nav-height bg-black
+    relative">
+      <div className="h-5/6 w-full flex-center flex-col" >
+        <p className="hero-title">
+          iPhone 15 pro
+          </p>
+        <div className="md:w-10/12 w-9/12" >
+        <video className="pointer-events-none"
+         autoPlay muted playsInline={true} key={videoSrc} >
+          <source src={videoSrc} type="video/mp4" />
+        </video>
+        </div>
+      </div>
+      <div
+      id="cta"
+      className="flex flex-col items-center opacity-0
+      translate-y-20">
+        <a href="#highlights" className="btn">Buy</a>
+        <p className="font-normal text-xl">From $199/month or $999</p>
+      </div>
+    </section>
+  )
+}
+
+export default Hero;
---
+++ src/main.jsx
@@ -0,0 +1,32 @@
+import React, { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+//...
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://301e3f4fce4893e7523cbaa81762f5dd@o4508143916417024.ingest.us.sentry.io/4508143917858816",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect: React.useEffect,
+    }),
+    Sentry.replayIntegration(),
+  ],
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
